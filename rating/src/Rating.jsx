944a5dd0e838a47051eff43ee0ccb7e3d8e62ee0
jenifer-xavi <jeniferx@stgit.com>
2026-04-22 10:41:48 +0530

    updated rating component

--- rating/src/Rating.jsx
+++ rating/src/Rating.jsx
@@ -5,6 +5,7 @@ const Rating=()=>{
     const[rating,setRating]=useState(0);
     const[hover,setHover]=useState(0);
 
+    let messages=["","Terrible","Bad","Okay","Good","Excellent"];
     return <div>
         <h2>Rating Component</h2>
         {
@@ -17,6 +18,12 @@ const Rating=()=>{
                 color: item<=(hover||rating)?"orange":"gray"
             }}>★</span>)
         }
+
+  <p>
+  {rating > 0
+    ? `${messages[rating]} (${rating}/5)`
+    : "No rating selected"}
+</p>
     </div>
 
 }
